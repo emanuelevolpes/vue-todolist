2cabf69eb7e6c187b452bdc69caec6cb843b4312
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-15 17:21:23 +0100

    x side item

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+//Visualizzare a fianco ad ogni item ha una “x”: 
+//cliccando su di essa, il todo viene rimosso dalla lista.
 
 const { createApp } = Vue;
 
@@ -14,7 +15,7 @@ createApp({
                 },
                 {
                     text: 'Comprare il pane',
-                    done: false
+                    done: true
                 },
                 {
                     text: 'Andare in palestra',
