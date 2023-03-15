fe635f3eedb38fbf011f40686f398dd786ba1259
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-15 19:45:15 +0100

    addToDo solved

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 'use strict';
 
-//Visualizzare a fianco ad ogni item ha una “x”: 
-//cliccando su di essa, il todo viene rimosso dalla lista.
-
-// Predisporre un campo di input testuale e un pulsante “aggiungi”: 
-// cliccando sul pulsante, il testo digitato viene letto e utilizzato 
-// per creare un nuovo todo, che quindi viene aggiunto alla lista dei 
-// todo esistenti.
-
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            myInput: {
-                text:'',
-                done: false
-            },
+            myInput: '',   
             xOnToDo: '',
             toDoList: [
                 {
@@ -39,7 +28,8 @@ createApp({
             this.toDoList.splice(index, 1);
         },
         addToDo() {
-            this.toDoList.push(this.myInput);
+            this.toDoList.push({ text: this.myInput, done: false });
+            this.myInput = '';
         }
     }
 }).mount('#app');
