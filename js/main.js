23476d9fee292268b1e8ed176bdf1876e5f87b35
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-15 19:03:50 +0100

    milestones 2-3 this problem

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@
 //Visualizzare a fianco ad ogni item ha una “x”: 
 //cliccando su di essa, il todo viene rimosso dalla lista.
 
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: 
+// cliccando sul pulsante, il testo digitato viene letto e utilizzato 
+// per creare un nuovo todo, che quindi viene aggiunto alla lista dei 
+// todo esistenti.
+
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
+            myInput: {
+                text:'',
+                done: false
+            },
+            xOnToDo: '',
             toDoList: [
                 {
                     text: 'Rifare documentazione Vue',
@@ -15,13 +25,21 @@ createApp({
                 },
                 {
                     text: 'Comprare il pane',
-                    done: true
+                    done: false
                 },
                 {
                     text: 'Andare in palestra',
                     done: false
                 }
             ]
+        }
+    },
+    methods: {
+        removeToDo() {
+            console.log(this.toDoList)
+        },
+        addToDo() {
+            this.toDoList.push(this.myInput);
         }
     }
 }).mount('#app');
