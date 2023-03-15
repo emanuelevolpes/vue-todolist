c896b2b3973f9e8e1dfd63d10c75d00455402bab
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-15 16:57:10 +0100

    item in to html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            toDoList: [
+                {
+                    text: 'Rifare documentazione Vue',
+                    done: false
+                },
+                {
+                    text: 'Comprare il pane',
+                    done: false
+                },
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                }
+            ]
+        }
+    }
+}).mount('#app');
